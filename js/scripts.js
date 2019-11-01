23e6f58797febc14e3a4e29d79b7350fab33610e
Lara Bjork <lara.m.bjork@gmail.com>
2019-11-01 13:47:06 -0700

    convert robotArray to string and split resulting string into separate elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,23 @@
-var robotWords = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop!", "Beep!"];
+var robotWords = ["I'm sorry, Dave. I'm afraid I can't do that", "Boop!", "Beep!"];
 var robotArray = []
 // business logic
 
 var robotCounter = function(num) {
   if (num < 1) {
     return "DANGER! DANGER! Does not compute! You must enter a number greater than 0!";
-  } else {
-    for (var i = 0; i <=num; i+= 1) {
-      if (i <= num && i !==3) {
-        robotArray.push(i)
-      } else {
-        robotArray.push(robotWords[0])
-      };
-    console.log(robotArray)
+} else {
+  for (var i = 0; i <=num; i+= 1) {
+    robotArray.push(i)
     };
+  // console.log(robotArray)
   };
+  var stringRobot = robotArray.toString();
+  var splitString = stringRobot.split();
+  console.log(stringRobot)
+  return splitString;
+
+
 };
-
-
-// Original version of second spec - with user logic mixed in?
-// } else {
-//   for (var i = 0; i <=num; i+= 1) {
-//     if (i < num) {
-//       $(".result").append(i + ", ")
-//     } else {
-//       $(".result").append(i)
-//     }
 
 
 
@@ -47,3 +39,22 @@ $(document).ready(function() {
    $(".outcome").text(result);
  });
 });
+
+//
+// THIS IS THE VERSION THAT WORKED TO SIMPLY SUBSTITUTE FOR INTEGER 3.
+// var robotCounter = function(num) {
+//   if (num < 1) {
+//     return "DANGER! DANGER! Does not compute! You must enter a number greater than 0!";
+//   } else {
+//     for (var i = 0; i <=num; i+= 1) {
+//       if (i <= num && i !==3) {
+//         robotArray.push(i)
+//       } else {
+//         robotArray.push(robotWords[0])
+//       };
+//       console.log(robotArray)
+//     };
+//   };
+//   return robotArray;
+// };
+//
