460010f1bf99d6d09f1a9fdcd75ecb126d98e77b
Lara Bjork <lara.m.bjork@gmail.com>
2019-11-03 10:25:29 -0800

    reincorporate for loop that pushes to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,56 @@
 var robotWords = ["I'm sorry, Dave. I'm afraid I can't do that", "Boop!", "Beep!"];
 var robotArray = [];
 
-// function robotCounter(num) {
-//   var analyzer = robotAnalyzer(num)
-//   robotAnalyzer.forEach (function (num){
-//     var banana = robotAnalyzer(num);
+// function mrRoboto(num) {
+//   var botCount = robotCounter(num)
+//   robotCounter.forEach (function (num){
+//     var banana = robotCounter(num);
 //     robotArray.push(banana);
 //   });
+//   return robotArray;
 // };
+
+var humanCounter = function(number) {
+  if (number < 1) {
+      return "DANGER! DANGER! Does not compute! You must enter a number greater than 0!";
+    } else {
+      for (var i = 0; i <=number; i+= 1) {
+        robotArray.push(i)
+      };
+    };
+  return robotArray;  
+}
+
+
 
 
 // business logic
 
-var robotCounter = function robotizer(num) {
-  if (num.match(/\d*3\d*/g)){
-    return robotThree(num)
-  } else if (num.match(/\d*2\d*/g)) {
-      return robotTwo(num)
-  } else if (num.match(/\d*1|d*/g)) {
-      return robotOne(num)
-  } else {
-      return num};
-  };
-
-
-
-
-function robotThree(num) {
-  return "I'm sorry, Dave. I'm afraid I can't do that"
-};
-
-function robotTwo(num) {
-  return "Boop!"
-};
-
-function robotOne(num) {
-  return "beep!"
-};
+// var robotCounter = function robotizer(num) {
+//   if (num.match(/\d*3\d*/g)){
+//     return robotThree(num)
+//   } else if (num.match(/\d*2\d*/g)) {
+//       return robotTwo(num)
+//   } else if (num.match(/\d*1\d*/g)) {
+//       return robotOne(num)
+//   } else {
+//       return num};
+//   };
+//
+//
+//
+//
+// function robotThree(num) {
+//   return "I'm sorry, Dave. I'm afraid I can't do that"
+// };
+//
+// function robotTwo(num) {
+//   return "Boop!"
+// };
+//
+// function robotOne(num) {
+//   return "beep!"
+// };
 
 
 
@@ -51,7 +65,8 @@ $(document).ready(function() {
    // $("div.result").toggle();
 
    var num = $("input#entry").val();
-   var result = robotCounter(num);
+   var number = parseInt($("input#entry").val());
+   var result = humanCounter(num);
 
    $(".outcome").text(result);
  });
@@ -63,20 +78,20 @@ $(document).ready(function() {
 
 // THIS CODE WORDS TO REPLACE SINGLE DIGITS 1, 2, or 3 in the TEXT==where I left off on Friday
 // var robotCounter = function(num) {
-  //   if (num < 1) {
-    //     return "DANGER! DANGER! Does not compute! You must enter a number greater than 0!";
-    //   } else {
-      //     for (var i = 0; i <=num; i+= 1) {
-        //       if (i <= num && i !==3) {
-          //         robotArray.push(i)
-          //       } else {
-            //         robotArray.push(robotWords[0])
-            //       };
-            //       console.log(robotArray)
-            //     };
-            //   };
-            //   return robotArray;
-            // };
+//     if (num < 1) {
+//         return "DANGER! DANGER! Does not compute! You must enter a number greater than 0!";
+//       } else {
+//           for (var i = 0; i <=num; i+= 1) {
+//               if (i <= num && i !==3) {
+//                   robotArray.push(i)
+//                 } else {
+//                     robotArray.push(robotWords[0])
+//                   };
+//                   console.log(robotArray)
+//                 };
+//               };
+//               return robotArray;
+//             };
 
 
 
